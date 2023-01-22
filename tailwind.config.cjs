58a4962148e3896e4364172c63bfbdaf4c1bc06a
RagnarokMew <temmieisalmightygod@gmail.com>
2023-01-22 20:11:45 +0200

    Add colour (generic) to config & example to style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* Light Theme Colour Palette */
+        'primary': '',
+        'secondary': '',
+        'accent1': '',
+        'accent2': '',
+  
+        /* Dark Theme Colour Pallete*/
+        'primary-dark': '#1c0414',
+        'secondary-dark': '#14959c',
+        'accent1-dark': '',
+        'accent2-dark': '',
+      },
+    },
   },
   plugins: [],
 }
