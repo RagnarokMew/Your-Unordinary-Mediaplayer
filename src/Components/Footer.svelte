cc6890638fb8cbdc4d2dc7250ecf484805c5a583
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-01 14:49:21 +0200

    Add footer component to landing page (static)

---
+++ src/Components/Footer.svelte
@@ -0,0 +1,44 @@
+<script>
+    import "../app.css";
+
+    //Form code go here
+</script>
+
+<footer class="dark:bg-gray-800 bg-rose-100 dark:text-gray-100 text-black flex gap-28 justify-center align-middle p-5">
+    <div class="flex flex-col gap-3">
+        <a href="#" class="text-2xl">Your Unordinary Repository</a>
+        <div class="flex gap-2">
+            <h2 class="text-xl">Made by:</h2>
+            <div class="flex flex-col gap-1 text-xl dark:text-cyan-200 text-stone-900">
+                <a href="https://github.com/RagnarokMew" target="_blank">[icon] RagnarokMew</a>
+                <a href="https://github.com/Ropler6" target="_blank">[icon] Ropley6</a>
+            </div>
+        </div>
+    </div>
+    <p class="text-2xl">Copyright Statement</p>
+    <div class="flex flex-col gap-1 text-lg">
+        <div>
+            Made with: [Icons]
+        </div>
+        <p>Contact us: [email]</p>
+        <div>
+            Sign up for our newsletter:
+            <form class="mt-2">
+                <input type="email" class="p-1 rounded dark:bg-gray-600 dark:text-gray-200 mb-3 outline-none">
+                <input type="submit" class="bg-blue-400 hover:bg-blue-450 active:bg-blue-600 py-1 px-2 rounded ml-2 text-white">
+            </form>
+        </div>
+    </div>
+</footer>
+
+<style>
+    p
+    {
+        display: flex;
+        align-items: center;
+    }
+    footer
+    {
+        width: 100%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+    import Footer from "../Components/Footer.svelte";
+</script>
+
+<main class="flex flex-col">
+    <div class="title">
+        <h1>Your Unordinary MediaPlayer</h1>
+        <h3>A novel way to listen to media, right in your browser</h3>
+    </div>
+    <div class="section2">
+        <h2>What we offer...</h2>
+        <div class="flex flex-col">
+
+        </div>
+    </div>
+    <div class="reviews">
+
+    </div>
+    <Footer/>
+</main>
+
+<style>
+    main{
+        width: 100%;
+    }
+</style>
