52aa259ed223c69c80b3db0b18b5b8415eb386d7
Ropler6 <robertmercea@gmail.com>
2023-02-14 14:39:11 +0200

    Add reactive rating amount

--- src/routes/forum/+page.svelte
+++ src/routes/forum/+page.svelte
@@ -5,6 +5,7 @@
 
     export let data: PageData;
     // export let form: ActionData;
+	let ratingAmount: number = 10;
 
     const questions: _Question[] = data.questions;
 
@@ -25,7 +26,7 @@
 <main class="w-full h-full grid grid-cols-3 grid-rows-[1fr,9fr]">
 	<div class="flex flex-col items-center justify-center row-span-1 col-start-1 col-end-4">
 		<h2>Your Unordinary Mediaplayer</h2>
-		<div class="flex flex-row">
+		<div class="flex flex-row gap-5">
 			<button on:click={toggleAddingQuestion} type="button">Ask a question</button>
 			<button on:click={toggleAddingReview} type="button">Leave a review</button>
 		</div>
@@ -41,8 +42,8 @@
 			<form method="POST" action="?/review" class="flex flex-col">
 				<label for="review">Review:</label>
 				<input type="text" name="review" id="question" />
-				<label for="rating">Rating:</label>
-				<input type="range" name="rating" min="1" max="10">
+				<label for="rating">Rating: {ratingAmount}</label>
+				<input bind:value={ratingAmount} type="range" name="rating" min="1" max="10">
 			</form>
 		{/if}
 	</div>
