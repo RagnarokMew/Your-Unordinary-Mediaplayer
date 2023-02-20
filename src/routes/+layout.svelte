07dd15e974484e7be84214debd74356e07eb93ee
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-20 23:51:26 +0200

    Add icon (svg image) to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,17 +13,17 @@
 
 </script>
 
-<main>
-    <nav>
-        <button>Expand</button>
-        <button class="toggle-dark">Theme Toggle</button>
-        <a href="#">Home</a>
-        <a href="#">Play</a>
-        <a href="#">Music</a>
-        <a href="#">Statistics</a>
-        <a href="#">Forum</a>
-        <a href="#">Account</a>
-        <a href="#">Settings</a>
+<main class="dark:bg-black dark:text-white">
+    <nav class="dark:bg-gray-900 bg-rose-200 shadow-sm shadow-rose-200/50 dark:shadow-gray-900/50 sticky top-0 left-0 h-screen p-7 font-extrabold gap-2">
+        <button> <img src="/Icons/menu.svg" class="w-10 h-10" alt="Expand"> </button>
+        <button class="toggle-dark">Theme</button>
+        <a href="#" class="w-10 h-10"> <img src="/Icons/home.svg" class="" alt="Home"> </a>
+        <a href="#"> <img src="/Icons/play.svg" class="w-10 h-10" alt="Play"> </a>
+        <a href="#"> <img src="/Icons/musical-notes.svg" class="w-10 h-10" alt="Music"> </a>
+        <a href="#"> <img src="/Icons/pie-chart.svg" class="w-10 h-10" alt="Statistics"> </a>
+        <a href="#"> <img src="/Icons/people.svg" class="w-10 h-10" alt="Forum"> </a>
+        <a href="#"> <img src="/Icons/home.svg" class="w-10 h-10" alt="Account"> </a>
+        <a href="#"> <img src="/Icons/settings.svg" class="w-10 h-10" alt="Settings"> </a>
     </nav>
 
     <slot />
@@ -39,12 +39,9 @@
     }
     nav
     {
-        border: 5px solid black;
-        padding: 20px;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        box-shadow: #ffffff 0px 5px 5px 0px;
     }
 </style>
