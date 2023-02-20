4ef0369edbd7a15f3b8ca79b5ab992e48db40bc3
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-21 00:13:44 +0200

    Link forum and landin page pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,11 +17,11 @@
     <nav class="dark:bg-gray-900 bg-rose-200 shadow-sm shadow-rose-200/50 dark:shadow-gray-900/50 sticky top-0 left-0 h-screen p-7 font-extrabold gap-2">
         <button> <img src="/Icons/menu.svg" class="w-10 h-10" alt="Expand"> </button>
         <button class="toggle-dark">Theme</button>
-        <a href="#" class="w-10 h-10"> <img src="/Icons/home.svg" class="" alt="Home"> </a>
+        <a href="/" class="w-10 h-10"> <img src="/Icons/home.svg" class="" alt="Home"> </a>
         <a href="#"> <img src="/Icons/play.svg" class="w-10 h-10" alt="Play"> </a>
         <a href="#"> <img src="/Icons/musical-notes.svg" class="w-10 h-10" alt="Music"> </a>
         <a href="#"> <img src="/Icons/pie-chart.svg" class="w-10 h-10" alt="Statistics"> </a>
-        <a href="#"> <img src="/Icons/people.svg" class="w-10 h-10" alt="Forum"> </a>
+        <a href="/forum"> <img src="/Icons/people.svg" class="w-10 h-10" alt="Forum"> </a>
         <a href="#"> <img src="/Icons/home.svg" class="w-10 h-10" alt="Account"> </a>
         <a href="#"> <img src="/Icons/settings.svg" class="w-10 h-10" alt="Settings"> </a>
     </nav>
