66b241cbdb06345f2a6796b446a8466aa03bbb5f
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-20 23:56:02 +0200

    Add review component for display purposes

---
+++ src/routes/Review.svelte
@@ -0,0 +1,22 @@
+<script>
+    /**
+    * ! For display purposes only
+    * * Styling and looks are not final
+    */
+    import "../app.css";
+
+    let score = 5;
+    let reviewer = "User";
+    let header = "Amazing";
+    let body = "My wife wanted to leave me, but after using YUM, we got back together. Truly a miracle. Thank you YUM";
+</script>
+
+<main class="flex flex-col w-1/5 bg-rose-200 p-3 dark:text-white dark:bg-gray-800">
+    <p class="">{reviewer} - {score}*</p>
+    <h3 class="text-xl font-bold">{header} ...</h3>
+    <p class="font-semibold">{body}</p>
+</main>
+
+<style>
+
+</style>
