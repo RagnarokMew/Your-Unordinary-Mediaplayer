6f2957c9ed996796bec99251acce552ed1c27e85
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-25 18:43:52 +0200

    Add more mock data (questions)

--- src/routes/forum/+page.server.ts
+++ src/routes/forum/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad, Actions } from "./$types";
 import type { Question, Review } from "$lib/interfaces"
 import { writeFile } from "node:fs"
+import { isDepsOptimizerEnabled } from "vite";
 
 //? Mock data
 let questions: Question[] = [
@@ -8,6 +9,16 @@ let questions: Question[] = [
         user: "Bob",
         question: "Test",
         details: "",
+    },
+    {
+        user: "Billy",
+        question: "How do use?",
+        details: "",
+    },
+    {
+        user: "User123",
+        question: "Hello, I think Internet is broken?",
+        details: "I try use YUM, but YUM no load and it say internet needed or smth. U SUCK. Get internet, make work. goodbye.",
     }
 ];
 
