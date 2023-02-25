b279a0ed628c07a308c89143c9ad2c3deb78ca0d
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-25 19:05:10 +0200

    Fix icon for Account Page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 </script>
 
-<main class="dark:bg-black dark:text-white">
+<main class="dark:bg-black dark:text-white flex flex-grow">
     <nav class="dark:bg-gray-900 bg-rose-200 shadow-sm shadow-rose-200/50 dark:shadow-gray-900/50 sticky top-0 left-0 h-screen p-7 font-extrabold gap-2">
         <button> <img src="/Icons/menu.svg" class="w-10 h-10" alt="Expand"> </button>
         <button on:click={toggleTheme} class="toggle-dark">Theme</button>
@@ -16,7 +16,7 @@
         <a href="/upload"> <img src="/Icons/musical-notes.svg" class="w-10 h-10" alt="Music"> </a>
         <a href="#"> <img src="/Icons/pie-chart.svg" class="w-10 h-10" alt="Statistics"> </a>
         <a href="/forum"> <img src="/Icons/people.svg" class="w-10 h-10" alt="Forum"> </a>
-        <a href="#"> <img src="/Icons/home.svg" class="w-10 h-10" alt="Account"> </a>
+        <a href="#"> <img src="/Icons/person.svg" class="w-10 h-10" alt="Account"> </a>
         <a href="#"> <img src="/Icons/settings.svg" class="w-10 h-10" alt="Settings"> </a>
     </nav>
 
