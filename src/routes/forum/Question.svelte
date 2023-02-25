c6189e4bf15d3dcb85ba1e86fdb22aee34ad876b
RagnarokMew <temmieisalmightygod@gmail.com>
2023-02-25 18:31:20 +0200

    Style Question component

--- src/routes/forum/Question.svelte
+++ src/routes/forum/Question.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-    
-    //TODO: the styling
+
+    /**
+        * TODO: Replace <img> with svg
+    */
 
     export let question = "Lorem ipsum dolor sit amet.";
     export let details = "Lorem ipsum dolor sit amet consectetur adipisicing elit. \
@@ -15,17 +17,21 @@
 
 </script>
 
-<main class="flex flex-col flex-wrap border-2 w-96 m-3 border-black">
+<main class="flex flex-col flex-wrap border-2 rounded-md shadow-md w-4/5 m-3 border-stone-400 dark:border-stone-500 bg-gray-200 dark:bg-gray-600">
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="w-96 border-black">
-        <button on:click={onClick} class="p-2 border-r-2 border-black">
-            V
+    <div class="border-black flex h-20">
+        <button on:click={onClick} class="w-20 p-2 border-r-2 border-stone-400 dark:border-stone-500 flex justify-center items-center">
+            {#if detailsVisible}
+                <img src="/Icons/caret-up.svg" alt="Show Less" class="w-12 h-12">
+            {:else}
+                <img src="/Icons/caret-down.svg" alt="Show More" class="w-12 h-12">
+            {/if}
         </button>
-        <div class="inline-block p-0 ml-2 mr-2 self-center">
-            Q: {question}
+        <div class="flex-grow p-0 ml-2 mr-2 self-center text-2xl font-semibold">
+            <span class="font-extrabold text-3xl italic">Q:</span> {question}
         </div>
     </div>
-    {#if detailsVisible}
-        <p class="text-clip border-t-2 border-black">{details}</p>
+    {#if detailsVisible && details != ''}
+        <p class="text-clip border-t-2 border-stone-400 dark:border-stone-500 p-5 font-medium italic text-base">{details}</p>
     {/if}
 </main>
