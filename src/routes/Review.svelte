bc406d33e49db762f48598e549a6f8d6f8c81a59
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-17 18:57:39 +0300

    Let Review component receives params

--- src/routes/Review.svelte
+++ src/routes/Review.svelte
@@ -5,14 +5,14 @@
     */
     import "../app.css";
 
-    let score = 5;
-    let reviewer = "User";
-    let header = "Amazing";
-    let body = "My wife wanted to leave me, but after using YUM, we got back together. Truly a miracle. Thank you YUM";
+    export let score = 10;
+    export let reviewer = "User";
+    export let header = "Amazing";
+    export let body = "My wife wanted to leave me, but after using YUM, we got back together. Truly a miracle. Thank you YUM";
 </script>
 
 <main class="flex flex-col w-1/5 bg-rose-200 p-3 dark:text-white dark:bg-gray-800">
-    <p class="">{reviewer} - {score}*</p>
+    <p class="">{reviewer} - {score/2}*</p>
     <h3 class="text-xl font-bold">{header} ...</h3>
     <p class="font-semibold">{body}</p>
 </main>
