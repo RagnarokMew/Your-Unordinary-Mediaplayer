e22beddbde6f8dd002aa533f6affaf01c1eb2d6f
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-24 23:04:14 +0300

    Remove forms & Add dynamic styling

--- src/routes/forum/+page.svelte
+++ src/routes/forum/+page.svelte
@@ -10,12 +10,8 @@
 </script>
 
 <main class="w-full h-screen flex flex-col">
-	<div class="flex flex-col items-center justify-center mb-3 dark:bg-gray-800 bg-rose-100">
-		<h2 class="flex justify-center items-center text-5xl font-extrabold my-5">Your Unordinary Mediaplayer - Forum</h2>
-		<div class="flex divide-x-2 divide-gray-800 dark:divide-rose-100 mb-3">
-			<a href="/forum/question" class="text-3xl font-extrabold p-2">Ask a question</a>
-			<a href="/forum/review" class="text-3xl font-extrabold p-2">Leave a review</a>
-		</div>
+	<div class="flex flex-col items-center justify-center mb-3 secondary-bg">
+		<h2 class="flex justify-center items-center text-5xl font-extrabold my-5">Your Unordinary Mediaplayer - FAQ</h2>
 	</div>
 	<!-- Question container -->
 	<div class="flex flex-col flex-grow items-center justify-center gap-2 py-7">
