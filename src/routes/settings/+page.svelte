53e8fd2d51bd84a7986db9726526e2603a3d3a67
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-25 13:59:00 +0300

    Add required settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,28 +2,48 @@
 	import type { Settings } from "$lib/interfaces";
     import { settings } from "$lib/stores";
 	import { onDestroy } from "svelte";
-
+    
     const localStorage = window.localStorage;
     const savedSettings = localStorage.getItem("settings");
     if (!savedSettings) {
         localStorage.setItem("settings", JSON.stringify({
-            mainLightColour: "#fecdd3",
-            secondaryLightColour: "#e5e7eb",
+            primaryLightColour: "#ffffff",
+            secondaryLightColour: "#efe6e6",
+            secondary2LightColour: "#cac1ba",
+            secondary3LightColour: "#93989f",
             accentLightColour: "#000000",
-            mainDarkColour: "#000000",
-            secondaryDarkColour: "#ffffff",
+            fontLightColour: "#000000",
+            rangeLightColour: "#cac1ba",
+            itemsLightColour: "#ffffff",
+            primaryDarkColour: "#111115",
+            secondaryDarkColour: "#404055",
+            secondary2DarkColour: "#2d2d3a",
+            secondary3DarkColour: "#1d1d24",
             accentDarkColour: "#000000",
+            fontDarkColour: "#ffffff",
+            rangeDarkColour: "#c09967",
+            itemsDarkColour: "#69697d",
             forwardTime: 15,
             backwardTime: 15,
         } satisfies Settings ));
 
         $settings = {
-            mainLightColour: "#fecdd3",
-            secondaryLightColour: "#e5e7eb",
+            primaryLightColour: "#ffffff",
+            secondaryLightColour: "#efe6e6",
+            secondary2LightColour: "#cac1ba",
+            secondary3LightColour: "#93989f",
             accentLightColour: "#000000",
-            mainDarkColour: "#000000",
-            secondaryDarkColour: "#ffffff",
+            fontLightColour: "#000000",
+            rangeLightColour: "#cac1ba",
+            itemsLightColour: "#ffffff",
+            primaryDarkColour: "#111115",
+            secondaryDarkColour: "#404055",
+            secondary2DarkColour: "#2d2d3a",
+            secondary3DarkColour: "#1d1d24",
             accentDarkColour: "#000000",
+            fontDarkColour: "#ffffff",
+            rangeDarkColour: "#c09967",
+            itemsDarkColour: "#69697d",
             forwardTime: 15,
             backwardTime: 15,
         }
@@ -31,12 +51,22 @@
     else {
         const parsedSavedSettings = JSON.parse(savedSettings);
         $settings = {
-            mainLightColour: parsedSavedSettings.mainLightColour,
+            primaryLightColour: parsedSavedSettings.primaryLightColour,
             secondaryLightColour: parsedSavedSettings.secondaryLightColour,
-            accentLightColour: parsedSavedSettings.accentLightColour,
-            mainDarkColour: parsedSavedSettings.mainDarkColour,
+            secondary2LightColour: parsedSavedSettings.secondary2LightColour,
+            secondary3LightColour: parsedSavedSettings.secondary3LightColour,
+            accentLightColour: parsedSavedSettings.accentLightColor,
+            fontLightColour: parsedSavedSettings.fontLightColour,
+            rangeLightColour: parsedSavedSettings.rangeLightColour,
+            itemsLightColour: parsedSavedSettings.itemsLightColour,
+            primaryDarkColour: parsedSavedSettings.primaryDarkColour,
             secondaryDarkColour: parsedSavedSettings.secondaryDarkColour,
+            secondary2DarkColour: parsedSavedSettings.secondary2DarkColour,
+            secondary3DarkColour: parsedSavedSettings.secondary3DarkColour,
             accentDarkColour: parsedSavedSettings.accentDarkColour,
+            fontDarkColour: parsedSavedSettings.fontDarkColour,
+            rangeDarkColour: parsedSavedSettings.rangeDarkColour,
+            itemsDarkColour: parsedSavedSettings.itemsDarkColour,
             forwardTime: parseInt(parsedSavedSettings.forwardTime),
             backwardTime: parseInt(parsedSavedSettings.backwardTime),
         }
@@ -55,23 +85,80 @@
 </script>
 
 
-<main class="flex flex-col">
-    <label for="mainLightColour">Main light mode color:</label>
-    <input on:change={updateSettings} id="mainLightColour" type="color">
-    <label for="secondaryLightColor">Secondary light mode color:</label>
-    <input on:change={updateSettings} id="secondaryLightColour" type="color">
-    <label for="accentLightColor">Accent light mode color:</label>
-    <input on:change={updateSettings} id="accentLightColour" type="color">
+<main class="flex flex-col divide-y-4 w-1/2 p-4">
+    <div class="font-bold text-4xl p-2">
+        Settings
+    </div>
+    <!--Light Colours-->
+    <div class="flex flex-col gap-1 p-4 divide-y-4">
+        <div class="font-bold text-3xl">
+            Themes
+        </div>
 
-    <label for="mainDarkColor">Main dark mode color:</label>
-    <input on:change={updateSettings} id="mainDarkColour" type="color">
-    <label for="secondaryDarkColor">Secondary dark mode color:</label>
-    <input on:change={updateSettings} id="secondaryDarkColour" type="color">
-    <label for="accentDarkColor">Accent dark mode color:</label>
-    <input on:change={updateSettings} id="accentDarkColour" type="color">
+        <div class="flex flex-col gap-2">
+            <label for="primaryLightColour">Primary Colour</label>
+            <input on:change={updateSettings} id="primaryLightColour" type="color">
+            <label for="secondaryLightColour">Secondary Colour 1</label>
+            <input on:change={updateSettings} id="secondaryLightColour" type="color">
+            <label for="secondary2LightColour">Secondary Colour 2</label>
+            <input on:change={updateSettings} id="secondary2LightColour" type="color">
+            <label for="secondary3LightColour">Secondary Colour 3</label>
+            <input on:change={updateSettings} id="secondary3LightColour" type="color">
+            <label for="accentLightColour">Accent Colour</label>
+            <input on:change={updateSettings} id="accentLightColour" type="color">
+            <label for="fontLightColour">Text / Icons Colour</label>
+            <input on:change={updateSettings} id="fontLightColour" type="color">
+            <label for="rangeLightColour">Progress Bar Colour</label>
+            <input on:change={updateSettings} id="rangeLightColour" type="color">
+            <label for="itemsLightColour">Song Bubble Colour</label>
+            <input on:change={updateSettings} id="itemsLightColour" type="color">
+        </div>
 
-    <label for="forwardTime">Forward time:</label>
-    <input value="15" on:change={updateSettings} id="forwardTime" type="number">
-    <label for="backwardTime">Backward time:</label>
-    <input value="15" on:change={updateSettings} id="backwardTime" type="number">
+        <div class="font-bold text-2xl p-4">
+            Light Mode
+        </div>
+
+        
+
+        <div class="font-bold text-2xl">
+            Dark Mode
+        </div>
+
+        <label for="primaryDarkColour">Primary Colour</label>
+        <input on:change={updateSettings} id="primaryDarkColour" type="color">
+        <label for="secondaryDarkColour">Secondary Colour 1</label>
+        <input on:change={updateSettings} id="secondaryDarkColour" type="color">
+        <label for="secondary2LightColour">Secondary Colour 2</label>
+        <input on:change={updateSettings} id="secondary2LightColour" type="color">
+        <label for="secondary3LightColour">Secondary Colour 3</label>
+        <input on:change={updateSettings} id="secondary3LightColour" type="color">
+        <label for="accentDarkColour">Accent Colour</label>
+        <input on:change={updateSettings} id="accentDarkColour" type="color">
+        <label for="fontDarkColour">Text / Icons Colour</label>
+        <input on:change={updateSettings} id="fontDarkColour" type="color">
+        <label for="rangeDarkColour">Progress Bar Colour</label>
+        <input on:change={updateSettings} id="rangeDarkColour" type="color">
+        <label for="itemsDarkColour">Song Bubble Colour</label>
+        <input on:change={updateSettings} id="itemsDarkColour" type="color">
+    </div>
+
+    <!--Dark Colours-->
+    <div class="flex flex-col gap-1">
+        
+    </div>
+
+    <div class="flex flex-col gap-1">
+        <label for="forwardTime">Forward time:</label>
+        <input value="15" on:change={updateSettings} id="forwardTime" type="number">
+        <label for="backwardTime">Backward time:</label>
+        <input value="15" on:change={updateSettings} id="backwardTime" type="number">
+    </div>
+
 </main>
+
+<style>
+    label
+    {
+        font-weight: 600;
+    }
+</style>
