ca763b3b5de66baa85e796c7a296698c1311ee18
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-25 23:49:56 +0300

    Add rounded border to reviews

--- src/routes/Review.svelte
+++ src/routes/Review.svelte
@@ -11,7 +11,7 @@
     export let body = "My wife wanted to leave me, but after using YUM, we got back together. Truly a miracle. Thank you YUM";
 </script>
 
-<main class="flex flex-col w-1/5 secondary2-bg p-3">
+<main class="flex flex-col w-1/5 secondary2-bg p-3 rounded-md">
     <p class="">{reviewer} - {score/2}*</p>
     <h3 class="text-xl font-bold">{header} ...</h3>
     <p class="font-semibold">{body}</p>
