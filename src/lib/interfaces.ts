7c3017cd7371c51dc4a395824e793306ea4538e9
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-25 13:26:33 +0300

    Implement colour settings interfaces + values

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -34,12 +34,22 @@ export interface Playlist {
 }
 
 export interface Settings {
-    mainLightColour: string;
+    primaryLightColour: string;
     secondaryLightColour: string;
+    secondary2LightColour: string;
+    secondary3LightColour: string;
     accentLightColour: string;
-    mainDarkColour: string;
+    fontLightColour: string;
+    rangeLightColour: string;
+    itemsLightColour: string;
+    primaryDarkColour: string;
     secondaryDarkColour: string;
+    secondary2DarkColour: string;
+    secondary3DarkColour: string;
     accentDarkColour: string;
+    fontDarkColour: string;
+    rangeDarkColour: string;
+    itemsDarkColour: string;
     forwardTime: number;
     backwardTime: number;
 }
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,12 +4,22 @@ import type { Playlist, Settings, SongData } from "./interfaces";
 const songsData = writable<SongData[]>(new Array<SongData>());
 const playlists = writable<Playlist[]>(new Array<Playlist>());
 const settings = writable<Settings>({
-    mainLightColour: "#fecdd3",
-    secondaryLightColour: "#e5e7eb",
+    primaryLightColour: "#ffffff",
+    secondaryLightColour: "#efe6e6",
+    secondary2LightColour: "#cac1ba",
+    secondary3LightColour: "#93989f",
     accentLightColour: "#000000",
-    mainDarkColour: "#000000",
-    secondaryDarkColour: "#ffffff",
+    fontLightColour: "#000000",
+    rangeLightColour: "#cac1ba",
+    itemsLightColour: "#ffffff",
+    primaryDarkColour: "#111115",
+    secondaryDarkColour: "#404055",
+    secondary2DarkColour: "#2d2d3a",
+    secondary3DarkColour: "#1d1d24",
     accentDarkColour: "#000000",
+    fontDarkColour: "#ffffff",
+    rangeDarkColour: "#c09967",
+    itemsDarkColour: "#69697d",
     forwardTime: 15,
     backwardTime: 15,
 });
