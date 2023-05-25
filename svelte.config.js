dde3be3b30e420454f9e826374bb2bcdf77222c9
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-25 18:15:58 +0300

    Add set-up for Render hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
-import cloudflare from "@sveltejs/adapter-cloudflare";
+//import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// /** @type {import('@sveltejs/kit').Config} */
+//const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+//	preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: cloudflare()
-	}
+//	kit: {
+//		adapter: adapter(),
+//	}
+//};
+
+//export default config;
+
+import adapter from '@sveltejs/adapter-node';
+
+export default {
+	preprocess: [vitePreprocess()],
+    kit: {
+        adapter: adapter(),
+    }
 };
-
-export default config;
