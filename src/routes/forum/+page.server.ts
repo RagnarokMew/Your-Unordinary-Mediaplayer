ead1c930ba7ca1038a8db1ddb09df06d73d003ba
RagnarokMew <temmieisalmightygod@gmail.com>
2023-05-26 20:16:25 +0300

    Remove unused/garbage code in faq server

--- src/routes/forum/+page.server.ts
+++ src/routes/forum/+page.server.ts
@@ -1,7 +1,4 @@
-import type { PageServerLoad, Actions } from "./$types";
-import type { Question, Review } from "$lib/interfaces"
-import { writeFile } from "node:fs"
-import { isDepsOptimizerEnabled } from "vite";
+import type { Question } from "$lib/interfaces"
 
 //? FAQ
 let questions: Question[] = [
@@ -47,55 +44,8 @@ let questions: Question[] = [
     },
 ];
 
-let reviews: Review[] = [
-    {
-        user: "James",
-        review: "Amazing!!!",
-        rating: 10,
-    }
-]
-
 export const load = () => {
     return {
         questions: questions
     }
 }
-
-export const actions = {
-    question: async({cookies, request}) => {
-        //TODO: get the user asking the question
-        //TODO: add the question to the database
-
-        const data = await request.formData();
-        const userQuestion = data.get("question");
-        const userDetails = data.get("details")
-
-        questions.push(
-            {
-                user: "Bob", 
-                question: (userQuestion?.toString() ?? ""), 
-                details: (userDetails?.toString() ?? ""),
-            }
-        )
-
-        return { success: true };
-    },
-
-    review: async({cookies, request}) => {
-        //TODO: add a review to the database
-        const data = await request.formData();
-        const userReview = data.get("review");
-        const userRating = data.get("rating");
-
-        reviews.push(
-            {
-                user: "James",
-                review: (userReview?.toString() ?? ""),
-                rating: (parseInt(userRating?.toString() ?? "10"))
-            }
-        )
-
-        return { success: true };
-    }
-
-} satisfies Actions;
